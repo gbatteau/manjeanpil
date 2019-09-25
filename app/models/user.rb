d128a32644cc4b2820f0d9be5004f5a5b67d144d
jordanhudgens <jordan@devcamp.com>
2019-09-25 13:22:45 -0700

    Extended the user model so that it can work with programatically created users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,19 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :plan
   has_one :profile
-  
-  # current_sign_in_ip
-  #geocoded_by :current_sign_in_ip, :latitude => :lat, :longitude => :long
-  #after_validation :geocode
-  
-  
-  #ip look up added gb
- 
-  
+
   geocoded_by :ip_lookup, :latitude => :lat, :longitude => :long
   after_validation :geocode
-  
+
   private
-  
+
   def ip_lookup
-    ip = Thread.current[:request].remote_ip
+    ip = Thread.current[:request].try(:remote_ip)
+
     if ip == '127.0.0.1' || ip == '::1'
-      '169.254.187.96/16'
+      '169.254.187.96'
     else
       ip
     end

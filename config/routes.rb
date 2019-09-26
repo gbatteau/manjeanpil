f8ebed185201bd45586ef660de2f00d5b0acbd5f
jordanhudgens <jordan@devcamp.com>
2019-09-26 13:16:05 -0700

    Removed the place generator from seeds and refactored the routes so that the root rout is at the bottom and won't conflict with other routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
-
-  root to: 'pages#home'
   devise_for :users, controllers: { registrations: 'users/registrations' }
-  
   resources :users do
   resource :profile
   resources :users, only: [:show]
-  end
-  
   resources :locations
-  
   get 'about', to: 'pages#about'
   resources :contacts, only: :create
   resources :posts
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
+  root to: 'pages#home'
 end

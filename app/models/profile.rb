b76067c079f5359d0eca51b551c32db819d94036
jordanhudgens <jordan@devcamp.com>
2019-09-26 13:19:07 -0700

    Added a dependent destroy flag to the profile relationship to prevent errors in the future

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,19 +3,18 @@ class Profile < ActiveRecord::Base
   belongs_to :user
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-  
+
   geocoded_by :address
   after_validation :geocode
-  
+
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode
-  
   validates_presence_of :restaurant_name
   validates_presence_of :street
   validates_presence_of :city
   validates_presence_of :state
   validates_presence_of :zipcode
-  
+
   def address
     [street, city, state, zipcode].compact.join(', ')
   end

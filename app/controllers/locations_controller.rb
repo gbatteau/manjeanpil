a4f39e15c222b87b6addc943e995f3dfb7d4b02e
jordanhudgens <jordan@devcamp.com>
2019-09-28 19:11:40 -0700

    Extended the functionality of the search bar to keep track of the location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,11 +1,11 @@
 class LocationsController < ApplicationController
   def index
     if params[:location].present?
-        @profiles =  Profile.near(params[:location], 50)
+      @profiles = Profile.near(params[:location], 50)
     elsif current_user && current_user.lat && current_user.long
-        @profiles =  Profile.near([current_user.lat, current_user.long], 50)
+      @profiles = Profile.near([current_user.lat, current_user.long], 50)
     else
-        @profiles = Profile.all
+      @profiles = Profile.last(20)
     end
   end
 end

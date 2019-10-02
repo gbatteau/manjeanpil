28b488bde3ecbb9d2e9f5330252e3ab6b7502c98
jordanhudgens <jordan@devcamp.com>
2019-10-01 17:43:27 -0700

    Fixed geocoding issue for the post model since there isn't a lat/long column in that table

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_attached_file :image, styles: { large: "600X600>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-  geocoded_by :address, :latitude => :lat, :longitude => :long
+  geocoded_by :address
   after_validation :geocode, :if => :address_changed?
 
   def address

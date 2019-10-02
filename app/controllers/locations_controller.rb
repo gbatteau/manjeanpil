bcc4ccc967c1748d4faefa53e090215d599aded8
jordanhudgens <jordan@devcamp.com>
2019-10-01 17:41:48 -0700

    Refactored the locations controller and view to utilize posts instead of profiles

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,11 +1,11 @@
 class LocationsController < ApplicationController
   def index
     if params[:location].present?
-      @profiles = Profile.near(params[:location], 50)
+      @posts = Post.near(params[:location], 50)
     elsif current_user && current_user.lat && current_user.long
-      @profiles = Profile.near([current_user.lat, current_user.long], 50)
+      @posts = Post.near([current_user.lat, current_user.long], 50)
     else
-      @profiles = Profile.last(20)
+      @posts = Post.last(20)
     end
   end
 end

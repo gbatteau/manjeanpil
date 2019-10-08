b94e4ad1a1672d37510329cb717123f186742238
jordanhudgens <jordan@devcamp.com>
2019-10-08 09:03:26 -0700

    Integrated the counter module into the new post show action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_post, only: [:edit, :update, :show]
+  impressionist actions: [:show]
 
   def index
     if(params.has_key?(:special_type))
@@ -11,6 +12,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    impressionist @post
   end
 
   def new

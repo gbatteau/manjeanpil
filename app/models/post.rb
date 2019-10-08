f4398f058f225acc300703f90b8f7d8b62e83117
jordanhudgens <jordan@devcamp.com>
2019-10-08 09:10:11 -0700

    Passed data to the post show, including the new counter value

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   has_attached_file :image, styles: { large: "600X600>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
+  is_impressionable counter_cache: true
+
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode
 
